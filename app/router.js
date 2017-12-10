a51f12cb488e6adcf30c0b1b8b4bbecb611e22ff
Matt Sroufe <mattsroufe@gmail.com>
2017-12-10 15:02:07 +1300

    add films

--- app/router.js
+++ app/router.js
@@ -7,6 +7,7 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
+  this.route('films');
 });
 
 export default Router;
---
+++ tests/acceptance/list-films-test.js
@@ -0,0 +1,19 @@
+import { test } from 'qunit';
+import moduleForAcceptance from 'video-store/tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | list films');
+
+test('visiting /list-films', function(assert) {
+  visit('/films');
+
+  andThen(function() {
+    assert.equal(currentURL(), '/films');
+  });
+});
+
+test('should list available rentals.', function (assert) {
+  visit('/films');
+  andThen(function() {
+    assert.equal(find('h1').text(), 'Films', 'should have a heading');
+  });
+});
